5388b56eee01fda3813e0d09c0c38e990b4bb143
Norcross <norcross@reaktivstudios.com>
2013-09-28 21:06:37 -0400

    initial commit

---
+++ readme.txt
@@ -0,0 +1,60 @@
+=== WP Comment Notes ===
+Contributors: norcross
+Website Link: http://andrewnorcross.com/plugins/
+Donate link: https://andrewnorcross.com/donate
+Tags: api keys, secret keys
+Requires at least: 3.0
+Tested up to: 3.5
+Stable tag: 1.0.0
+License: GPLv2 or later
+License URI: http://www.gnu.org/licenses/gpl-2.0.html
+
+Add custom notes before or after the comment form.
+
+== Description ==
+
+Add custom notes before or after the comment form.
+
+
+== Installation ==
+
+This section describes how to install the plugin and get it working.
+
+1. Upload `wp-comment-notes` to the `/wp-content/plugins/` directory.
+2. Activate the plugin through the 'Plugins' menu in WordPress.
+3. Add some text in the fields on a post
+4. That's it.
+
+
+== Frequently Asked Questions ==
+
+
+= How does it work? =
+
+Activate the plugin and the metaboxes will appear for posts. Use the filter to add more post type support.
+
+= Where is the settings menu? =
+
+There isn't one. On purpose.
+
+= They don't show up on my theme. =
+
+Then you're using a theme / framework that has gone cowboy and done it their own way. I can't do anything about that.
+
+
+== Screenshots ==
+
+1.
+
+
+
+== Changelog ==
+
+= 1.0 =
+* First release!
+
+
+== Upgrade Notice ==
+
+= 1.0 =
+* First release!
---
+++ wp-comment-notes.php
@@ -0,0 +1,314 @@
+<?php
+/*
+Plugin Name: WP Comment Notes
+Plugin URI: http://andrewnorcross.com/plugins/
+Description: Add custom notes before or after the comment form.
+Version: 1.0.0
+Author: Andrew Norcross
+Author URI: http://andrewnorcross.com
+
+	Copyright 2013 Andrew Norcross
+
+	This program is free software; you can redistribute it and/or modify
+	it under the terms of the GNU General Public License, version 2, as
+	published by the Free Software Foundation.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU General Public License for more details.
+
+	You should have received a copy of the GNU General Public License
+	along with this program; if not, write to the Free Software
+	Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
+*/
+
+if( !defined( 'WPCMN_VER' ) )
+	define( 'WPCMN_VER', '1.0.0');
+
+// Start up the engine
+class WP_Comment_Notes
+{
+
+	/**
+	 * Static property to hold our singleton instance
+	 *
+	 * @return WP_Comment_Notes
+	 */
+	static $instance = false;
+
+	/**
+	 * This is our constructor
+	 *
+	 * @return WP_Comment_Notes
+	 */
+	private function __construct() {
+		// back end
+		add_action		( 'plugins_loaded', 					array( $this, 'textdomain'				) 			);
+		add_action		( 'admin_enqueue_scripts',				array( $this, 'admin_scripts'			)			);
+		add_action		( 'do_meta_boxes',						array( $this, 'create_metaboxes'		),	10,	2	);
+		add_action		( 'save_post',							array( $this, 'save_custom_meta'		),	1		);
+
+		// front end
+		add_action		( 'wp_enqueue_scripts',					array( $this, 'front_scripts'			)			);
+		add_filter		( 'comment_form_defaults',				array( $this, 'custom_notes_filter'		) 			);
+	}
+
+	/**
+	 * If an instance exists, this returns it.  If not, it creates one and
+	 * retuns it.
+	 *
+	 * @return WP_Comment_Notes
+	 */
+
+	public static function getInstance() {
+		if ( !self::$instance )
+			self::$instance = new self;
+		return self::$instance;
+	}
+
+	/**
+	 * load textdomain
+	 *
+	 * @return WP_Comment_Notes
+	 */
+
+	public function textdomain() {
+
+		load_plugin_textdomain( 'wpcmn', false, dirname( plugin_basename( __FILE__ ) ) . '/languages/' );
+
+	}
+
+	/**
+	 * Admin styles
+	 *
+	 * @return WP_Comment_Notes
+	 */
+
+	public function admin_scripts() {
+
+		// set post types with filter
+		$types	= array( 'post' );
+		$types	= apply_filters( 'wpcmn_type_support', $types );
+
+		$screen	= get_current_screen();
+
+		if ( in_array( $screen->base , $types ) ) :
+
+			wp_enqueue_style( 'wpcmn-admin', plugins_url('lib/css/admin.css', __FILE__), array(), WPCMN_VER, 'all' );
+
+		endif;
+
+	}
+
+	/**
+	 * call metabox
+	 *
+	 * @return WP_Comment_Notes
+	 */
+
+	public function create_metaboxes( $page, $context ) {
+
+		// set post types with filter
+		$types	= array( 'post' );
+		$types	= apply_filters( 'wpcmn_type_support', $types );
+
+		if ( !is_array( $types ) )
+			$types	= array ( $types );
+
+		if ( in_array( $page,  $types ) )
+			add_meta_box( 'wp-comment-notes', __( 'Comment Notes', 'wpcmn' ), array( $this, 'wpcmn_notes_meta' ), $page, 'advanced', 'high' );
+
+	}
+
+	/**
+	 * display meta fields for notes meta
+	 *
+	 * @return WP_Comment_Notes
+	 */
+
+	public function wpcmn_notes_meta( $post ) {
+
+		// Use nonce for verification
+		wp_nonce_field( 'wpcmn_meta_nonce', 'wpcmn_meta_nonce' );
+
+		$post_id	= $post->ID;
+
+		// get postmeta, and our initial settings
+		$notes		= get_post_meta( $post_id, '_wpcmn_notes', true );
+
+		$before_text	= isset( $notes['before-text'] )	? $notes['before-text']	: '';
+		$before_type	= isset( $notes['before-type'] )	? $notes['before-type']	: 'wpcmn-notes-standard';
+		$after_text		= isset( $notes['after-text'] )		? $notes['after-text']	: '';
+		$after_type		= isset( $notes['after-type'] )		? $notes['after-type']	: 'wpcmn-notes-standard';
+
+		echo '<table class="form-table wpcmn-notes-table">';
+
+			echo '<tr class="wpcmn-notes-title">';
+			echo '<td colspan="2"><h5>'.__( 'Before Notes Area', 'wpcmn' ).'</h5></td>';
+			echo '</tr>';
+
+			echo '<tr class="wpcmn-notes-data wpcmn-notes-before-text">';
+				echo '<th>'.__( 'Message Text', 'wpcmn' ).'</th>';
+				echo '<td>';
+					echo '<textarea class="widefat" name="wpcmn-notes[before-text]" id="wpcmn-before">'.esc_attr( $before_text ).'</textarea>';
+					echo '<p class="description">'.__( 'Note: This will not appear to users who are logged in.', 'wpcmn' ).'</p>';
+				echo '</td>';
+			echo '</tr>';
+
+			echo '<tr class="wpcmn-notes-data wpcmn-notes-before-type">';
+				echo '<th>'.__( 'Message Type', 'wpcmn' ).'</th>';
+				echo '<td>';
+					echo '<select id="wpcmn-before-type" name="wpcmn-notes[before-type]">';
+					echo '<option value="wpcmn-notes-standard" '.selected( $before_type, 'wpcmn-notes-standard', false ).'>'.__('Standard', 'wpcmn').'</option>';
+					echo '<option value="wpcmn-notes-warning" '.selected( $before_type, 'wpcmn-notes-warning', false ).'>'.__('Warning', 'wpcmn').'</option>';
+					echo '<option value="wpcmn-notes-alert" '.selected( $before_type, 'wpcmn-notes-alert', false ).'>'.__('Alert', 'wpcmn').'</option>';
+					echo apply_filters( 'wpcmn_before_types', $before_type );
+					echo '</select>';
+				echo '</td>';
+			echo '</tr>';
+
+			echo '<tr class="wpcmn-notes-title">';
+			echo '<td colspan="2"><h5>'.__( 'After Notes Area', 'wpcmn' ).'</h5></td>';
+			echo '</tr>';
+
+			echo '<tr class="wpcmn-notes-data wpcmn-notes-after-text">';
+				echo '<th>'.__( 'Message Text', 'wpcmn' ).'</th>';
+				echo '<td>';
+					echo '<textarea class="widefat" name="wpcmn-notes[after-text]" id="wpcmn-after">'.esc_attr( $after_text ).'</textarea>';
+				echo '</td>';
+			echo '</tr>';
+
+			echo '<tr class="wpcmn-notes-data wpcmn-notes-after-type">';
+				echo '<th>'.__( 'Message Type', 'wpcmn' ).'</th>';
+				echo '<td>';
+					echo '<select id="wpcmn-after-type" name="wpcmn-notes[after-type]">';
+					echo '<option value="wpcmn-notes-standard" '.selected( $after_type, 'wpcmn-notes-standard', false ).'>'.__('Standard', 'wpcmn').'</option>';
+					echo '<option value="wpcmn-notes-warning" '.selected( $after_type, 'wpcmn-notes-warning', false ).'>'.__('Warning', 'wpcmn').'</option>';
+					echo '<option value="wpcmn-notes-alert" '.selected( $after_type, 'wpcmn-notes-alert', false ).'>'.__('Alert', 'wpcmn').'</option>';
+					echo apply_filters( 'wpcmn_after_types', $after_type );
+					echo '</select>';
+				echo '</td>';
+			echo '</tr>';
+
+		echo '</table>';
+
+
+	}
+
+	/**
+	 * save post metadata
+	 *
+	 * @return WP_Backstretch_Post_Meta
+	 */
+
+	public function save_custom_meta( $post_id ) {
+
+		// make sure we aren't using autosave
+		if ( defined( 'DOING_AUTOSAVE' ) && DOING_AUTOSAVE )
+			return $post_id;
+
+		// do our nonce check. ALWAYS A NONCE CHECK
+		if ( ! isset( $_POST['wpcmn_meta_nonce'] ) || ! wp_verify_nonce( $_POST['wpcmn_meta_nonce'], 'wpcmn_meta_nonce' ) )
+			return $post_id;
+
+		// check post types with filter
+		$types	= array( 'post' );
+		$types	= apply_filters( 'wpcmn_type_support', $types );
+
+		if ( !in_array ( $_POST['post_type'], $types ) )
+			return $post_id;
+
+		// and make sure the user has the ability to do shit
+		if ( 'page' == $_POST['post_type'] ) :
+
+			if ( !current_user_can( 'edit_page', $post_id ) ) :
+				return $post_id;
+			endif;
+
+		else:
+
+			if ( !current_user_can( 'edit_post', $post_id ) ) :
+				return $post_id;
+			endif;
+
+		endif;
+
+		// all clear. get data via $_POST and store it
+		$notes	= $_POST['wpcmn-notes'];
+
+		// update side meta data
+		if ( isset( $notes ) && !empty( $notes ) ) :
+			update_post_meta( $post_id, '_wpcmn_notes', $notes );
+		else:
+			delete_post_meta( $post_id, '_wpcmn_notes' );
+		endif;
+
+	}
+
+
+	/**
+	 * call front-end CSS
+	 *
+	 * @return WP_Comment_Notes
+	 */
+
+	public function front_scripts() {
+
+		// set post types with filter
+		$types	= array( 'post' );
+		$types	= apply_filters( 'wpcmn_type_support', $types );
+
+		if ( is_singular( $types ) )
+			wp_enqueue_style( 'wpcmn-notes', plugins_url( 'lib/css/wpcmn-notes.css', __FILE__ ), array(), WPCMN_VER, 'all' );
+
+	}
+
+	/**
+	 * The actual filter for adding the notes.
+	 *
+	 * @return WP_Comment_Notes
+	 */
+
+	public function custom_notes_filter( $fields ) {
+
+		global $post;
+
+		// get the possible meta fields
+		$notes	= get_post_meta( $post->ID, '_wpcmn_notes', true );
+
+		if ( empty( $notes ) )
+			return $fields;
+
+		if ( isset( $notes['before-text'] ) ) :
+			// grab the variables
+			$text	= $notes['before-text'];
+			$class	= isset( $notes['before-type'] ) ? $notes['before-type'] : 'wpcmn-notes-standard';
+			// build the string
+			$before	= '<p class="wpcmn-notes wpcmn-notes-before '.$class.'">'.esc_attr( $text ).'</p>';
+			// output
+			$fields['comment_notes_before'] = $before;
+
+		endif;
+
+		if ( isset( $notes['after-text'] ) ) :
+			// grab the variables
+			$text	= $notes['after-text'];
+			$class	= isset( $notes['after-type'] ) ? $notes['after-type'] : 'wpcmn-notes-standard';
+			// build the string
+			$after	= '<p class="wpcmn-notes wpcmn-notes-after '.$class.'">'.esc_attr( $text ).'</p>';
+			// output
+			$fields['comment_notes_after'] = $after;
+
+		endif;
+
+
+		return $fields;
+	}
+
+/// end class
+}
+
+
+// Instantiate our class
+$WP_Comment_Notes = WP_Comment_Notes::getInstance();
